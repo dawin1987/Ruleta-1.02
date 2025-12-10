02a6415ae7cff95000059ec88bae67d5ab31b358
dawin1987 <dawinalmanzar@gmail.com>
2025-12-10 13:59:35 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE = "ruleta-cache-v1";
+const FILES = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
